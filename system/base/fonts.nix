3c6dfbf14a6914ee40ee6df0304ab3851ddf64bf
ViliLuosujarvi <viluosuj@edu.lapinamk.fi>
2025-08-13 18:50:43 +0300

    cahnges to font.nix

--- system/base/fonts.nix
+++ system/base/fonts.nix
@@ -12,7 +12,23 @@
 	font-awesome
 	victor-mono
 	iosevka-bin
-	#nerdfonts
+	noto-fonts-color-emoji
 
+	#nerdfonts
+	nerd-fonts.fira-code
+	nerd-fonts.fira-code-symbols
+	nerd-fonts.iosevka
+	nerd-fonts.mononoki
   ];
+
+  fontconfig = {
+    enable = true;
+    defaultFonts = {
+      serif = [ "Fira Code" ];
+      sansSerif = [ "Fira Code" ];
+      monospace = [ "Fira Code" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };  
+
 }
