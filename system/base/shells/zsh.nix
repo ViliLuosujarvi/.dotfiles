a0b301a3d38f461b38615ae03884371a3e32b6a3
ViliLuosujarvi <viluosuj@edu.lapinamk.fi>
2025-08-16 03:23:39 +0300

    minor tweaks to configs

--- system/base/shells/zsh.nix
+++ system/base/shells/zsh.nix
@@ -56,7 +56,7 @@
   #  ./../../../.config/oh-my-zsh/custom/themes; 
  
   environment.etc."oh-my-zsh/custom/themes/zsh-theme".source =
-  ${self}/.dotfiles/hosts/Home-Desktop/.config/oh-my-zsh/custom/themes/mytheme.zsh-theme;
+  .dotfiles/hosts/Home-Desktop/.config/oh-my-zsh/custom/themes/mytheme.zsh-theme;
   
 
   # Set ZSH as default shell for nansus
