b0ad84f17aec775c3933969c388420b7cd35923f
ViliLuosujarvi <vili.luosujarvi@edu.lapinamk.fi>
2025-09-10 08:32:56 +0300

    changed nixos hardware for laptop to 16iah7 config

--- flake.nix
+++ flake.nix
@@ -74,7 +74,7 @@
           inherit system;
           modules = [
              "${self}/hosts/Laptop/configuration.nix"
-	    nixos-hardware.nixosModules.lenovo-legion-16ithg6
+	    nixos-hardware.nixosModules.lenovo-legion-16iah7h
       	    ./system/base/shells/zsh.nix
           ];
           specialArgs = {
@@ -86,7 +86,7 @@
           inherit system;
           modules = [
              "${self}/hosts/Work/Home-Laptop-Work/configuration.nix"
-	   nixos-hardware.nixosModules.lenovo-legion-16ithg6
+	   nixos-hardware.nixosModules.lenovo-legion-16iah7h
       	   ./system/base/shells/zsh.nix
           ];
           specialArgs = {
