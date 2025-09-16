6eb8bb8c354e831c13fb443336c8b2473b6d1e33
ViliLuosujarvi <viluosuj@edu.lapinamk.fi>
2025-09-16 09:19:03 +0300

    enabled FiraCode as kitty font

--- system/base/fonts.nix
+++ system/base/fonts.nix
@@ -2,25 +2,52 @@
 
 {
   # Add fonts to system
-  fonts.packages = with pkgs;
-     map (font: font.overrideAttrs {preferLocalBuild = true;}) [
+  #fonts.packages = with pkgs;
+     #map (font: font.overrideAttrs {preferLocalBuild = true;}) [
 	#roboto
 	#noto-fonts
 	#noto-fonts-cjk-sans
 	#noto-fonts-cjk-serif
 	#twemoji-color-font
-	font-awesome
+	#font-awesome
 	#victor-mono
 	#iosevka-bin
-	noto-fonts-color-emoji
+	#noto-fonts-color-emoji
 
 	#nerdfonts
-	nerd-fonts.fira-code
-	nerd-fonts.fira-mono
-	nerd-fonts.iosevka
-	nerd-fonts.mononoki
+	#nerd-fonts.fira-code
+	#nerd-fonts.fira-mono
+	#nerd-fonts.iosevka
+	#nerd-fonts.mononoki
+	
+  #];
 
-  ];
+  # Add fonts to system
+  font = {
+	fontDir.enable = true;
+	enableGhostscriptFonts = true;
+	packages = with pkgs; [
+     		map (font: font.overrideAttrs { fonts = [ "FiraCode" "DroidSansMono" "JetBrainsMono" ]; ) [
+		#roboto
+		#noto-fonts
+		#noto-fonts-cjk-sans
+		#noto-fonts-cjk-serif	
+		#twemoji-color-font
+		font-awesome
+		#victor-mono
+		#iosevka-bin
+		noto-fonts-color-emoji
+
+		#nerdfonts
+		nerd-fonts.fira-code
+		nerd-fonts.fira-mono
+		nerd-fonts.iosevka
+		nerd-fonts.mononoki
+		];
+ 	};	
+
+
+
 
   fonts.fontconfig = {
     enable = true;
