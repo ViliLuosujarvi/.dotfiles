4df2b69373a9c5d03a7f3ae8bb7f96bdc8d90f44
ViliLuosujarvi <viluosuj@edu.lapinamk.fi>
2025-10-19 03:15:37 +0300

    took a little time to kinda clean and comment the falek.nix file a bit and such

--- flake.nix
+++ flake.nix
@@ -1,39 +1,69 @@
 {
-  description = "NanSuS Desktop flake";
+  # might be too much to describe what happens in description
+  description = "NanSuS' flake for my own computers and servers";
 
+  # pkgs imported from git and such
   inputs = {
+    # nix pkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
+    # Home-mnanger and it is set to follow smae nixplgs version
+    # as nixpkgs.url has been set to follow
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Most of my machines can be found on nixos-hardware
+    # repo so I'm using them there. if you are more interested,
+    # Slave is custom built computer which doesn't have 
+    # configuration in nixos-hardware, check:
+    # .dotfiles/system/hardware/nividia_proprietary.nix or
+    # https://nixos.wiki/wiki/Nvidia for more info
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
+    # flake utils, might need.
     flake-utils.url = "github:numtide/flake-utils";
 
+    # prettier cursor, change with your
+    # own preference as they can be found
+    # in myriad forms in git repos
     rose-pine-hyprcursor = {
       url = "github:ndom91/rose-pine-hyprcursor";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.hyprlang.follows = "hyprland/hyprlang";
     };
 
+    # Import hyprland even tough I am pretty sure
+    # that I know in my heart that this doens't do shit
+    # in my configuration because I have been so lazy to 
+    # not change hyprland.nix file in system/base/ :(
     hyprland = {
       url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprlang = {
-      url = "github:hyprwm/hyprlang";
-      inputs.system.follows = "system";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # not in use at the moment, could be just
+    # removed from this file entirely
+    #hyprlang = {
+    #  url = "github:hyprwm/hyprlang";
+    #  inputs.system.follows = "system";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
 
+    # sops-nix for ecrets management
+    # (still under maintanance aka I haven't had
+    # time nor motivaion to learn this yet so far 
+    # to actually implement it as safe solution)
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
+    # VSCode server for running e.g jupyter server for
+    # machine learning purposes
     vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
+  # Here the machine output that is result of
+  # inputs given above and a modular set of .nix files
+  # other pkgs, tadaa!
   outputs = {
 
       self,
@@ -49,25 +79,32 @@
      
       }@inputs:
 
-
+    # "let in" couple of more used blocks of informations
+    # like system versions as $system and so on to
+    # nixosConfiguration and homeConfiguration.
     let
 
       # ---- SYSTEM SETTINGS ----
         system = "x86_64-linux";
         lib = nixpkgs.lib;
-        #pkgs = nixpkgs.legacyPackages."x86_64-linux";
         pkgs = import nixpkgs {
 	     inherit system;
+           # Allow unfree because I happen
+	   # to still use software that is not
+	   # all open-source, my bad!
   	   config.allowUnfree = true;
 	};
     in {
 
+      # Nixos configuration set for Home-Desktop,
+      # Laptop and Slave. Anything else is a demo
+      # until further declared
       nixosConfigurations = {
 	# Leisure configuration for Home-Desktop
         Home-Desktop = lib.nixosSystem {
           inherit system;
           modules = [
-             "${self}/hosts/Home-Desktop/configuration.nix"
+             "${self}/hosts/Home-Desktop/configuration.nix
              nixos-hardware.nixosModules.lenovo-legion-t526amr5
              ./system/base/shells/zsh.nix
 	     sops-nix.nixosModules.sops
@@ -130,6 +167,10 @@
              "${self}/hosts/Slave/configuration.nix"
       	    ./system/base/shells/zsh.nix
 	    sops-nix.nixosModules.sops
+	    
+	    # in case you are wondering, Slave machine
+	    # is declader in it's configuration.nix as 
+	    # in .dotfiles/system/hardware/nvidia/proprietary.nix
 
 	    # remember to run this on host machine:
 	    # systemctl --user enable auto-fix-vscode-server.service
@@ -144,18 +185,28 @@
         };
       };
 
+      # Home configuration for Home-Desktop Laptop and Slave.
+      # Anything else is demo testing:
+
+      # Basic information regarding all home configurations 
       homeConfigurations = let basics = {
               home.username = "nansus";
               home.homeDirectory = "/home/nansus";
               home.stateVersion = "24.11";
 	      
+	      # enable home manager
               programs.home-manager.enable = true;
               };
           in {
+
+	# unerneath are all different configurations
+	# for the given machines and their home.nix
+	# files can be found from each ones own hosts/
+	# section in the .dotfiles
         "nansus@Home-Desktop" = inputs.home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
-	      		"${self}/hosts/Home-Desktop/home.nix"
+	      	"${self}/hosts/Home-Desktop/home.nix"
   	      	basics
           ];
         };
@@ -163,7 +214,7 @@
 	"nansus@Home-Desktop-Work" = inputs.home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
-	     		"${self}/hosts/Work/Home-Desktop-Work/home.nix"
+	     	"${self}/hosts/Work/Home-Desktop-Work/home.nix"
   	     	basics
           ];
         };
@@ -171,15 +222,15 @@
         "nansus@Home-Laptop" = inputs.home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
-    	      "${self}/hosts/Laptop/home.nix"
-	        	basics
+    	      	"${self}/hosts/Laptop/home.nix"
+	        basics
           ];
         };
 
         "nansus@Home-Laptop-Work" = inputs.home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
-    	     "${self}/hosts/Work/Home-Laptop-Work/home.nix"
+    	     	"${self}/hosts/Work/Home-Laptop-Work/home.nix"
 	       	basics
           ];
         };
@@ -187,8 +238,8 @@
         "nansus@Slave" = inputs.home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
-	      		"${self}/hosts/Slave/home.nix"
-						basics
+	      	"${self}/hosts/Slave/home.nix"
+		basics
           ];
         };
       };
