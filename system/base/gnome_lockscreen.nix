be72516c921e73cc2ee01b47ab545f4813ca652b
ViliLuosujarvi <viluosuj@edu.lapinamk.fi>
2025-10-20 17:31:28 +0300

    yuh

--- system/base/gnome_lockscreen.nix
+++ system/base/gnome_lockscreen.nix
@@ -1,10 +1,16 @@
 {pkgs, ...}:
 
 {
+
+  # You may change gnome lockscreen to something else, but think of it as the
+  # same type of effect as with the The Garden of Earthly delights, bland exterior
+  # and colorful interior
+
   # Enable gnome based lockscreen
   services.xserver = {
      enable = true;
      displayManager.gdm.enable = true;
 
   };
+
 }
