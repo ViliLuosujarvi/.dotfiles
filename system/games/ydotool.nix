9d9b191ae67cc0fb3be17958527d007e8cdba0c5
ViliLuosujarvi <viluosuj@edu.lapinamk.fi>
2025-10-22 21:02:27 +0300

    added ydotool

--- system/games/ydotool.nix
+++ system/games/ydotool.nix
@@ -2,4 +2,22 @@
 
 {
   environment.systemPackages = [ pkgs.ydotool ];
+
+  # uinput permissions
+  services.udev.extraRules = ''
+    KERNEL=="uinput", MODE="0660", GROUP="input"
+  '';
+
+
+  users.users.nansus.extraGroups = [ "input" "uinput" ];
+
+  # optional: start ydotoold at boot for your user
+  systemd.user.services.ydotoold = {
+    description = "Ydotool daemon";
+    serviceConfig = {
+      ExecStart = "${pkgs.ydotool}/bin/ydotoold --socket-path=/run/user/%U/.ydotool_socket --socket-perm=0666";
+    };
+    wantedBy = [ "default.target" ];
+  };
+
 }
