e6a4d7125ac1ea4df785eb18248831a4ea3bba35
ViliLuosujarvi <viluosuj@edu.lapinamk.fi>
2025-12-11 10:43:57 +0200

    cleanups for .dotfiles in general

--- system/games/steam.nix
+++ system/games/steam.nix
@@ -6,13 +6,14 @@
   programs = {
        steam = {
           enable = true;
-  	  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  	  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  	  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+	  # Open ports in the firewall for Steam Remote Play
+  	  remotePlay.openFirewall = true;
+	  # Open ports in the firewall for Source Dedicated Server
+  	  dedicatedServer.openFirewall = true;
+	  # Open ports in the firewall for Steam Local Network Game Transfers
+  	  localNetworkGameTransfers.openFirewall = true;
        }; 
    };
-
-  programs.gamemode.enable = true;
 
   # Additional 32bit graphics driver
   hardware.opengl.driSupport32Bit = true;
