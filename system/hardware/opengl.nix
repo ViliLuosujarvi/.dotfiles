7d41c270f80b18b0f28158fc08c2fb7b4243beb5
ViliLuosujarvi <viluosuj@edu.lapinamk.fi>
2025-12-11 10:35:52 +0200

    cleanups for .dotfiles in general

--- system/hardware/opengl.nix
+++ system/hardware/opengl.nix
@@ -2,8 +2,15 @@
 
 {
   # OpenGL
-  hardware.opengl.enable = true;
-  hardware.opengl.extraPackages = with pkgs; [
+#  hardware.opengl.enable = true;
+#  hardware.opengl.extraPackages = with pkgs; [
+#    rocmPackages.clr.icd
+  ];
+
+  # OpenGL
+  hardware.graphics.enable = true;
+  hardware.graphics.extraPackages = with pkgs; [
     rocmPackages.clr.icd
   ];
+
 }
