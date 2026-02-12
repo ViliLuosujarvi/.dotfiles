3a4e6929b74b43c8d4af0ce5b63c3f917055c16c
ViliLuosujarvi <viluosuj@edu.lapinamk.fi>
2026-02-12 14:03:08 +0100

    added virtualbox

---
+++ system/app/virt-manager.nix
@@ -0,0 +1,9 @@
+
+programs.virt-manager.enable = true;
+
+users.groups.libvirtd.members = ["nansus"];
+
+virtualisation.libvirtd.enable = true;
+
+virtualisation.spiceUSBRedirection.enable = true;
+
