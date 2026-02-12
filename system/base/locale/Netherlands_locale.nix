932448b66d0867923560e8645527e9f472362a8a
ViliLuosujarvi <viluosuj@edu.lapinamk.fi>
2026-02-12 12:04:01 +0200

    adjusted locale setting for netherlands time

--- system/base/locale/Netherlands_locale.nix
+++ system/base/locale/Netherlands_locale.nix
@@ -8,14 +8,14 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "NL_NL.UTF-8";
-    LC_IDENTIFICATION = "NL_NL.UTF-8";
-    LC_MEASUREMENT = "NL_NL.UTF-8";
-    LC_MONETARY = "NL_NL.UTF-8";
-    LC_NAME = "NL_NL.UTF-8";
-    LC_NUMERIC = "NL_NL.UTF-8";
-    LC_PAPER = "NL_NL.UTF-8";
-    LC_TELEPHONE = "NL_NL.UTF-8";
-    LC_TIME = "NL_NL.UTF-8";
+    LC_ADDRESS = "nl_NL.UTF-8";
+    LC_IDENTIFICATION = "nl_NL.UTF-8";
+    LC_MEASUREMENT = "nl_NL.UTF-8";
+    LC_MONETARY = "nl_NL.UTF-8";
+    LC_NAME = "nl_NL.UTF-8";
+    LC_NUMERIC = "nl_NL.UTF-8";
+    LC_PAPER = "nl_NL.UTF-8";
+    LC_TELEPHONE = "nl_NL.UTF-8";
+    LC_TIME = "nl_NL.UTF-8";
   };
 }
